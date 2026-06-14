9fa7aa5d3d8df95785bf0026b2f62417287bfc80
yanbahiano07-hue <yanbahiano07@gmail.com>
2026-06-14 19:21:58 -0300

    Add converter-ASCII

---
+++ mini-projects/converter-ASCII.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+    int n;
+
+    printf("Digite uma caracter entre 1-127: ");
+    scanf("%d",&n);
+
+    printf("O codigo ASCII é: %c\n",n);
+
+    return 0;
+}
