1e43714c0fb02960611ecb939bed153429368bad
yanbahiano07-hue <yanbahiano07@gmail.com>
2026-06-18 16:13:42 -0300

    fix basic bug

--- mini-projects/basic-calculator.C
+++ mini-projects/basic-calculator.C
@@ -1,22 +1,21 @@
 #include <stdio.h>
 int main (){
-    int n1, n2, soma, sub, multi;
-    float  div;
+    float n1, n2, soma, sub, multi, div;
 
     printf("Digite o primeiro número: ");
-    scanf("%d",&n1);
+    scanf("%f",&n1);
 
     printf("Digite o segundo número: ");
-    scanf("%d",&n2);
+    scanf("%f",&n2);
 
     soma = n1 + n2;
     sub = n1-n2;
     multi = n1*n2;
     div = n1/n2;
     
-    printf("-----------------------------\nSoma = %d\n", soma);
-    printf("Subtração = %d\n", sub);
-    printf("Multiplicação = %d\n", multi);
+    printf("-----------------------------\nSoma = %f\n", soma);
+    printf("Subtração = %f\n", sub);
+    printf("Multiplicação = %f\n", multi);
     printf("Divisão = %f\n", div);
 
     return 0;
